5e5140605b5c880bec3945079de8e0676363764b
zohaibhassan02 <98820921+zohaibhassan02@users.noreply.github.com>
2023-05-17 14:21:02 -0400

    Add files via upload

---
+++ assign1/Initials.java
@@ -0,0 +1,19 @@
+/**
+@author Zohaib Khan 3740572
+**/
+
+public class Initials {
+  public static void main(String[] args) {
+    System.out.println(" * * * * * *     *     * ");
+    System.out.println("          *      *    * ");
+    System.out.println("         *       *   * ");
+    System.out.println("        *        *  * ");
+    System.out.println("       *         * * ");
+    System.out.println("      *          ** ");
+    System.out.println("     *           * * ");
+    System.out.println("    *            *  * ");
+    System.out.println("   *             *   * ");
+    System.out.println("  *              *    * ");
+    System.out.println(" * * * * * *     *     * ");
+  }
+}
